31583eb80e3e6ea5718463ec034f278fe0bf5225
riranrmarques-ctrl <riran.rmarques@gmail.com>
2026-04-27 17:23:01 -0300

    Update acesso.js

--- acesso.js
+++ acesso.js
@@ -47,8 +47,8 @@ const listaMateriais = document.getElementById("listaMateriais");
 const historicoStatusPonto = document.getElementById("historicoStatusPonto");
 const nomeClienteTopo = document.getElementById("nomeClienteTopo");
 
-const totalReproducoesCliente = document.getElementById("totalReproducoesCliente");
-const totalQrCodeCliente = document.getElementById("totalQrCodeCliente");
+document.getElementById("totalReproducoesCliente").textContent = "292";
+document.getElementById("totalQrCodeCliente").textContent = "13";
 const graficoReproducoesCliente = document.getElementById("graficoReproducoesCliente");
 const graficoQrCodeCliente = document.getElementById("graficoQrCodeCliente");
 
